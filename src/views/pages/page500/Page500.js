61783da807e799574125c3be8943d1c1d6bf8def
Jeisson Ibargüen Maturana <jeisim18@gmail.com>
2023-12-23 10:29:29 -0500

    remove noused component

--- src/views/pages/page500/Page500.js
+++ src/views/pages/page500/Page500.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import {
-  CButton,
   CCol,
   CContainer,
-  CFormInput,
-  CInputGroup,
-  CInputGroupText,
   CRow,
 } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilMagnifyingGlass } from '@coreui/icons'
 
 const Page500 = () => {
   return (
